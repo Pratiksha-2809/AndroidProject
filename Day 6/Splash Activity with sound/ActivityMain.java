09a034a77f0d96f645604acfa1ad4ecac19e154e
Pratiksha-2809 <pratikshap952@gmail.com>
2021-06-13 18:41:09 +0530

    Day 6

---
+++ Day 6/Splash Activity with sound/ActivityMain.java
@@ -0,0 +1,54 @@
+package com.example.splashactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.music);
+        mediaPlayer.start();
+
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.release();
+        finish();
+    }
+
+}
