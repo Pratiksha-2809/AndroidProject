27f5339fca26c8074d06f5179430013e77229797
Pratiksha-2809 <pratikshap952@gmail.com>
2021-06-14 14:42:03 +0530

    Day 10

---
+++ Day 10/Audio Play/MainActivity.java
@@ -0,0 +1,48 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonStart, buttonPause, buttonStop;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonPause = findViewById(R.id.buttonPause);
+        buttonStop = findViewById(R.id.buttonStop);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.song);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonPause.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonStart:
+                mediaPlayer.start();;
+                break;
+
+            case R.id.buttonPause:
+                mediaPlayer.pause();
+                break;
+
+            case  R.id.buttonStop:
+                mediaPlayer.stop();
+                break;
+
+        }
+
+    }
+}
